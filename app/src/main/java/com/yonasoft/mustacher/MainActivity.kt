60111a06b361b5c6bbfa842dd05679139071b132
KevC000 <chenkevin1119@gmail.com>
2024-05-21 22:10:13 -0400

    initial ARCore setup

--- app/src/main/java/com/yonasoft/mustacher/MainActivity.kt
+++ app/src/main/java/com/yonasoft/mustacher/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yonasoft.mustacher
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -9,13 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import com.yonasoft.mustacher.presentation.components.bottom_nav_bar.BottomNavBar
 import com.yonasoft.mustacher.presentation.screens.gallery_screen.GalleryScreen
 import com.yonasoft.mustacher.presentation.screens.recording_screen.RecordingScreen
@@ -31,7 +37,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MustacherApp()
+                    val context = LocalContext.current
+                    val arcoreAvailability = ArCoreApk.getInstance().checkAvailability(context).isSupported
+
+                    if(arcoreAvailability){
+                        var session: Session?= null
+                        var userRequestedInstall = true
+                        try{
+                            if (session == null){
+                                when(ArCoreApk.getInstance().requestInstall(this
+                                    , userRequestedInstall)){
+
+                                    ArCoreApk.InstallStatus.INSTALLED -> {
+                                        session = Session(this)
+                                        val config = Config(session)
+                                        session.configure(config)
+                                    }
+
+                                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                                        userRequestedInstall = false
+                                    }
+                                }
+                            }
+                        } catch (e: UnavailableUserDeclinedInstallationException) {
+                            // Display an appropriate message to the user and return gracefully.
+                            Toast.makeText(this, "TODO: handle exception $e", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        MustacherApp(session)
+                    }
+                    else {
+                        Text("AR Core not supported")
+                    }
                 }
             }
         }
@@ -39,7 +76,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MustacherApp () {
+fun MustacherApp (
+    session: Session?
+) {
 
     var selectedItemIndex by rememberSaveable {
         mutableIntStateOf(0)
@@ -58,20 +97,13 @@ fun MustacherApp () {
 
             when (selectedItemIndex) {
                 0 -> {
-                    RecordingScreen()
+                    RecordingScreen(session)
                 }
                 1 -> {
+                    session!!.close()
                     GalleryScreen()
                 }
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MustacherTheme {
-        MustacherApp()
-    }
-}
+ }
--- app/src/main/java/com/yonasoft/mustacher/presentation/screens/recording_screen/RecordingScreen.kt
+++ app/src/main/java/com/yonasoft/mustacher/presentation/screens/recording_screen/RecordingScreen.kt
@@ -2,13 +2,15 @@ package com.yonasoft.mustacher.presentation.screens.recording_screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.ar.core.Session
 
 @Composable
-fun RecordingScreen(){
+fun RecordingScreen(session: Session?){
+
     Box(modifier = Modifier.fillMaxSize()){
-        Text(text = "recording")
+
     }
 }
+
